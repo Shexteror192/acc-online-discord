03a068e424545088ed35f0641733bf3755057f7a
robotxvenom <70775408+robotxvenom@users.noreply.github.com>
2020-09-05 01:20:32 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,202 @@
+const Discord = require("discord.js");
+const myid = ['692482962809290823'];
+const developers = ['692482962809290823'];
+const prefix = ['tokyo#'];
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+const client26 = new Discord.Client();
+const client27 = new Discord.Client();
+const client28 = new Discord.Client();
+const client29 = new Discord.Client();
+const client30 = new Discord.Client();
+
+
+//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+
+
+client.login("");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+client7.login("");
+client8.login("");
+client9.login("");
+client10.login("");
+client11.login("");
+client12.login("");
+client13.login("");
+client14.login("");
+client15.login("");
+client16.login("");
+client17.login("");
+client18.login("");
+client19.login("");
+client20.login("");
+client21.login("");
+client22.login("");
+client23.login("");
+client24.login("");
+client25.login("");
+client26.login("");
+client27.login("");
+client28.login("");
+client29.login("");
+client30.login("");
+
+
+//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+
+
+
+client.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+
+client2.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+
+client3.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+
+client4.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+
+client5.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+
+client6.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+client7.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+client8.on('message', message => {
+var argresult = message.content.split().slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+let count = 0;
+let ecount = 0;
+for(let x = 0; x < 30000; x++) {
+message.channel.send(**??? ???? ??? ?? ??? **[ " ${x} " ])
+.then(m => {
+count++;
+})
+
+}
+}
+});
+client9.on('message', message => {
+var argresult = me
